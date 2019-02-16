1fe1c1d371a186f6a138f4cb22e3973fcb268a49
Markus Paasovaara <markus.paasovaara@futurice.com>
2019-02-16 17:20:46 +0200

    Improved the boilerplate template

--- src/integration-test/java/fi/hsl/transitdata/hfp/ITDeduplicatorTest.java
+++ src/integration-test/java/fi/hsl/transitdata/hfp/ITDeduplicatorTest.java
@@ -3,6 +3,7 @@ package fi.hsl.transitdata.hfp;
 import com.google.transit.realtime.GtfsRealtime;
 import fi.hsl.common.pulsar.ITBaseTestSuite;
 import fi.hsl.common.pulsar.PulsarApplication;
+import fi.hsl.common.pulsar.PulsarMessageData;
 import fi.hsl.common.transitdata.TransitdataProperties;
 import org.apache.pulsar.client.api.Message;
 import org.apache.pulsar.client.api.PulsarClientException;
@@ -10,9 +11,13 @@ import org.apache.pulsar.client.api.TypedMessageBuilder;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.ListIterator;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class ITDeduplicatorTest extends ITBaseTestSuite {
     @Test
@@ -20,43 +25,36 @@ public class ITDeduplicatorTest extends ITBaseTestSuite {
         final String testId = "-test-duplicates";
         PulsarApplication app = createPulsarApp("integration-test-dedup.conf", testId);
 
+        //TODO add all required compontents to constructor
         Deduplicator dedup = new Deduplicator(app.getContext(), null);
 
-        TestLogic logic = new TestLogic() {
+        ArrayList<PulsarMessageData> input = new ArrayList<>();
+        ArrayList<PulsarMessageData> output = new ArrayList<>();
+        for (int n = 0; n < 3; n++) {
+            long ts = System.currentTimeMillis();
+            String msg = "testme" + n;
+            String key = "jabadabaduu" + n;
 
-            @Override
-            public void testImpl(TestContext context) throws Exception {
-                long now = System.currentTimeMillis();
-                sendStringMessage(context, "key", "testme", now);
-                sendStringMessage(context, "key", "testme", now);
-                sendStringMessage(context, "key", "testme2", now);
-                logger.info("Message sent, reading it back");
-
-                Message<byte[]> received = readOutputMessage(context);
-                assertNotNull(received);
-                Message<byte[]> received2 = readOutputMessage(context);
-                assertNotNull(received2);
-
-                //validatePulsarProperties(received, "key", now, null);
-                String readData = new String(received.getData());
-                assertEquals("testme", readData);
-                String readData2 = new String(received2.getData());
-                assertEquals("testme2", readData2);
-                logger.info("Message read back, all good");
-
-                validateAcks(3, context);
+            PulsarMessageData data = new PulsarMessageData(msg.getBytes(), ts, key);
+            //varying number of inputs, but expecting each message only to arrive once.
+            //And the one with the first key as first. TODO
+            for (int times = 0; times <= n; times++) {
+                input.add(data);
             }
-        };
+            output.add(data);
+        }
+
+        BufferedTestLogic logic = new BufferedTestLogic(input, output);
         testPulsarMessageHandler(dedup, app, logic, testId);
     }
 
     public static class BufferedTestLogic extends TestLogic {
 
-        protected final ArrayList<TypedMessageBuilder<byte[]>> input;
-        protected final ArrayList<TypedMessageBuilder<byte[]>> expectedOutput;
+        protected final ArrayList<PulsarMessageData> input;
+        protected final ArrayList<PulsarMessageData> expectedOutput;
 
-        public BufferedTestLogic(ArrayList<TypedMessageBuilder<byte[]>> in,
-                                      ArrayList<TypedMessageBuilder<byte[]>> out) {
+        public BufferedTestLogic(ArrayList<PulsarMessageData> in,
+                                 ArrayList<PulsarMessageData> out) {
             input = in;
             expectedOutput = out;
         }
@@ -66,7 +64,9 @@ public class ITDeduplicatorTest extends ITBaseTestSuite {
             //For simplicity let's just send all messages first and then read them back.
             logger.info("Sending {} messages", input.size());
             long now = System.currentTimeMillis();
-            for(TypedMessageBuilder<byte[]> msg : input) {
+
+            for(PulsarMessageData inputData : input) {
+                TypedMessageBuilder<byte[]> msg = PulsarMessageData.toPulsarMessage(context.source, inputData);
                 msg.send();
             }
             logger.info("Messages sent in {} ms, reading them back", (System.currentTimeMillis() - now));
@@ -75,7 +75,7 @@ public class ITDeduplicatorTest extends ITBaseTestSuite {
             ArrayList<Message<byte[]>> buffer = new ArrayList<>();
             now = System.currentTimeMillis();
             while (buffer.size() < expectedCount) {
-                Message<byte[]> read = readOutputMessage(context);
+                Message<byte[]> read = ITBaseTestSuite.readOutputMessage(context);
                 assertNotNull("Was expecting more messages but got null!", read);
                 buffer.add(read);
             }
@@ -83,16 +83,47 @@ public class ITDeduplicatorTest extends ITBaseTestSuite {
 
             assertEquals(expectedCount, buffer.size());
             //All input messages should have been acked.
-            validateAcks(input.size(), context);
+            ITBaseTestSuite.validateAcks(input.size(), context);
 
             validateOutput(buffer);
         }
 
-        protected void validateOutput(ArrayList<Message<byte[]>> received) {
-            //Override this for your own custom check
+        protected void validateOutput(ArrayList<Message<byte[]>> receivedQueue) {
+            assertEquals(expectedOutput.size(), receivedQueue.size());
+            ListIterator<Message<byte[]>> itrRecv = receivedQueue.listIterator();
+            ListIterator<PulsarMessageData> itrExp = expectedOutput.listIterator();
 
+            while (itrRecv.hasNext()) {
+                Message<byte[]> receivedMsg = itrRecv.next();
+                PulsarMessageData received = PulsarMessageData.fromPulsarMessage(receivedMsg);
+                PulsarMessageData expected = itrExp.next();
+
+                validateMessage(expected, received);
+            }
         }
 
-
+        /**
+         * Override this for your own check if needed
+         */
+        protected void validateMessage(PulsarMessageData expected, PulsarMessageData received) {
+            assertNotNull(expected);
+            assertNotNull(received);
+            assertEquals(expected, received);
+            //assertTrue(equals(expected, received));
+        }
+        /*
+        public boolean equals(PulsarMessageData expected, PulsarMessageData received) {
+            if (!Arrays.equals(expected.payload, received.payload))
+                return false;
+            if (!received.key.equals(expected.key))
+                return false;
+            if (!received.eventTime.equals(expected.eventTime))
+                return false;
+            if (!received.properties.equals(expected.properties))
+                return false;
+            if (!received.schema.equals(expected.schema))
+                return false;
+            return true;
+        }*/
     }
 }
