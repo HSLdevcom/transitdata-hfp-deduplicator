7a03a151c6a81e218ee671f809d865ff124d1a09
Markus Paasovaara <markus.paasovaara@futurice.com>
2019-02-19 14:12:20 +0000

    Hfp test

--- src/integration-test/java/fi/hsl/transitdata/hfp/ITDeduplicatorTest.java
+++ src/integration-test/java/fi/hsl/transitdata/hfp/ITDeduplicatorTest.java
@@ -1,6 +1,9 @@
 package fi.hsl.transitdata.hfp;
 
+import com.google.protobuf.ByteString;
 import com.google.transit.realtime.GtfsRealtime;
+import fi.hsl.common.hfp.proto.Hfp;
+import fi.hsl.common.mqtt.proto.Mqtt;
 import fi.hsl.common.pulsar.ITBaseTestSuite;
 import fi.hsl.common.pulsar.PulsarApplication;
 import fi.hsl.common.pulsar.PulsarMessageData;
@@ -10,10 +13,11 @@ import org.apache.pulsar.client.api.PulsarClientException;
 import org.apache.pulsar.client.api.TypedMessageBuilder;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.ListIterator;
-import java.util.Map;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -24,19 +28,21 @@ public class ITDeduplicatorTest extends ITBaseTestSuite {
     public void testDummyDuplicatesWithoutSchema() throws Exception {
         final String testId = "-test-duplicate-strings";
         PulsarApplication app = createPulsarApp("integration-test-dedup.conf", testId);
-
-        //TODO add all required compontents to constructor
-        Deduplicator dedup = new Deduplicator(app.getContext(), null);
+        Deduplicator dedup = newDeduplicator(app);
 
         ArrayList<PulsarMessageData> input = new ArrayList<>();
         ArrayList<PulsarMessageData> output = new ArrayList<>();
         for (int n = 0; n < 10; n++) {
             long ts = System.currentTimeMillis();
+
             String msg = "testme" + n;
             //varying number of inputs, but expecting each message only to arrive once.
             for (int times = 0; times <= n; times++) {
                 //Vary the key to make sure we only receive the first one sent.
                 String key = "jabadabaduu" + times;
+                //Also timestamp should not matter, only the payload
+                ts =+ 1;
+
                 PulsarMessageData data = new PulsarMessageData(msg.getBytes(), ts, key);
 
                 input.add(data);
@@ -49,6 +55,156 @@ public class ITDeduplicatorTest extends ITBaseTestSuite {
 
         BufferedTestLogic logic = new BufferedTestLogic(input, output);
         testPulsarMessageHandler(dedup, app, logic, testId);
+    }
+
+    private Deduplicator newDeduplicator(PulsarApplication app) {
+        //TODO add all required compontents to constructor
+        return new Deduplicator(app.getContext(), null);
+    }
+
+    @Test
+    public void testDuplicateTimestamps() throws Exception {
+        //Different timestamps should not matter, only the data.
+        final String testId = "-test-hfp-duplicates";
+        PulsarApplication app = createPulsarApp("integration-test-dedup.conf", testId);
+
+        Deduplicator dedup = newDeduplicator(app);
+
+        ArrayList<PulsarMessageData> input = new ArrayList<>();
+        ArrayList<PulsarMessageData> output = new ArrayList<>();
+
+
+
+        BufferedTestLogic logic = new BufferedTestLogic(input, output);
+        testPulsarMessageHandler(dedup, app, logic, testId);
+    }
+
+    static class TopicAndPayload {
+        public TopicAndPayload(String t, String p) {
+            topic = t;
+            payload = p;
+        }
+        public String topic;
+        public String payload;
+        /*
+        @Override
+        public boolean equals(Object o) {
+            if (o == this)
+                return true;
+            TopicAndPayload other = (TopicAndPayload)o;
+            //Rely we're never null here, or if we are it's a bug and deserves to be thrown.
+            return topic.equals(other.topic) && payload.equals(other.payload);
+        }
+
+        @Override
+        public int hashCode() {
+            //Not relevant
+            return 1;
+        }*/
+
+        @Override
+        public String toString() {
+            return topic + " " + payload;
+        }
+    }
+
+    @Test
+    public void testDuplicatesWithRawMqttSchema() throws Exception {
+        final HashMap<String, String> properties = new HashMap<>();
+        properties.put(TransitdataProperties.KEY_PROTOBUF_SCHEMA, TransitdataProperties.ProtobufSchema.MqttRawMessage.toString());
+        properties.put(TransitdataProperties.KEY_SCHEMA_VERSION, "1");
+        final long ts = System.currentTimeMillis(); //Let's use the same timestamp for all
+
+        LinkedList<String> lines = readLinesFromResources("hfp-5000.txt");
+        assertEquals(5000, lines.size());
+
+        final List<Mqtt.RawMessage> sourcePayloads = lines.stream()
+                .map(ITDeduplicatorTest::parseMqttRawMessage)
+                .collect(Collectors.toList());
+        final List<Mqtt.RawMessage> uniquePayloads = new LinkedList<>();
+
+        HashMap<String, Integer> counter = new HashMap<>();
+        for (Mqtt.RawMessage raw: sourcePayloads) {
+            String key = raw.getTopic() + " " + new String(raw.getPayload().toByteArray());
+            Integer prevCount = counter.get(key);
+            if (prevCount == null) {
+                counter.put(key, 1);
+                uniquePayloads.add(raw);
+            } else {
+                logger.info("Duplicate: " + key);
+                counter.put(key, prevCount + 1);
+            }
+        }
+
+        final String testId = "-test-raw-mqtt-duplicates";
+        PulsarApplication app = createPulsarApp("integration-test-dedup.conf", testId);
+
+        Deduplicator dedup = newDeduplicator(app);
+
+        final ArrayList<PulsarMessageData> input = sourcePayloads.stream().map(raw -> {
+            byte[] data = raw.toByteArray();
+            return new PulsarMessageData(data, ts, "hfp"/*, properties*/);
+        }).collect(Collectors.toCollection(ArrayList::new));
+
+        final ArrayList<PulsarMessageData> output = uniquePayloads.stream().map(raw -> {
+            byte[] data = raw.toByteArray();
+            return new PulsarMessageData(data, ts, "hfp"/*, properties*/);
+        }).collect(Collectors.toCollection(ArrayList::new));
+
+        logger.info("Sending {} hfp messages and expecting {} back", input.size(), output.size());
+        BufferedTestLogic logic = new BufferedTestLogic(input, output);
+        testPulsarMessageHandler(dedup, app, logic, testId);
+    }
+
+    LinkedList<String> readLinesFromResources(String filename) throws Exception {
+        LinkedList<String> lines = new LinkedList<>();
+        BufferedReader reader = null;
+        try {
+            ClassLoader classLoader = getClass().getClassLoader();
+            URL url = classLoader.getResource(filename);
+            reader = new BufferedReader(new InputStreamReader(url.openStream()));
+
+            String line;
+            while ((line = reader.readLine()) != null)
+            {
+                lines.add(line);
+            }
+            // close our reader
+            reader.close();
+        }
+        finally {
+            if (reader != null)
+                reader.close();
+        }
+        return lines;
+    }
+
+    public static TopicAndPayload parseTopicAndPayload(String line) {
+        // Topic can unfortunately contain spaces so we need to parse the line using some heuristic methods.
+        String[] splitted = line.split(" ", 2);
+        final String serverTimestamp = splitted[0]; // not needed in this test
+        final String topicAndPayload = splitted[1];
+
+        int indexOfJsonStart = topicAndPayload.indexOf("{");
+
+        final String topic = topicAndPayload.substring(0, indexOfJsonStart).trim();
+        final String jsonPayload = topicAndPayload.substring(indexOfJsonStart);
+        return new TopicAndPayload(topic, jsonPayload);
+    }
+
+    public static Mqtt.RawMessage parseMqttRawMessage(String line) {
+        return parseMqttRawMessage(parseTopicAndPayload(line));
+    }
+
+    public static Mqtt.RawMessage parseMqttRawMessage(TopicAndPayload tp) {
+
+        Mqtt.RawMessage.Builder builder = Mqtt.RawMessage.newBuilder();
+        Mqtt.RawMessage raw = builder
+                .setSchemaVersion(builder.getSchemaVersion())
+                .setTopic(tp.topic)
+                .setPayload(ByteString.copyFrom(tp.payload.getBytes()))
+                .build();
+        return raw;
     }
 
     public static class BufferedTestLogic extends TestLogic {
